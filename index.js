6571ab5782d36c17918e273901f2d459e27f0c5c
Ihor-Slastonov <jammik000@gmail.com>
2023-02-12 01:34:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,52 @@ const {
   addContact,
 } = require('./contacts');
 
-const { Command } = require("commander");
+const { Command } = require('commander');
 const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const contacts = await getListContacts();
+      console.table(contacts);
+      break;
+
+    case 'get':
+      const contact = await getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(contact);
+      break;
+
+    case 'add':
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case 'remove':
+      const removeContact = await removeContactById(id);
+      if (!removeContact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+(async () => {
+  await invokeAction(argv);
+})();
